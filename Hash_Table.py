41f598ec9647cf8f8a4e1af03b118302a2d61e6e
yangjung-woo <yee777222000@gmail.com>
2024-05-11 11:12:41 +0900

    hashtable 이론 공부

---
+++ Hash_Table.py
@@ -0,0 +1,33 @@
+# 해시테이블 구현 방법 > 파이썬의 경우 딕셔너리로 구현되어있음 
+# 1. Array list 사용 > 탐색시 소요 시간 O(1)
+# 충돌시 Open addressing 방식으로 해결
+ 
+# dict[key] = value 
+d ={}
+
+d[0] = '양정우'
+d[1] = '김정우'
+d[7720] = '애앵정우' # directed-address Table : key값 그대로 인덱스로 사용, 단점: 자료공간 낭비 
+
+print(d)
+
+# 원래 이론상 해시테이블 
+
+def Hashfunction(key):
+    return key%8
+
+hash_d ={}
+
+hash_d[Hashfunction(1)]= '양정우'
+hash_d[Hashfunction(3)]= '정우'
+hash_d[Hashfunction(5)]= '양우'
+hash_d[Hashfunction(7)]= '양정'
+
+hash_d[Hashfunction(9)]= '우' # collision 발생 
+hash_d[Hashfunction(11)]= '정'
+hash_d[Hashfunction(13)]= '양'
+print(hash_d)
+
+# 딕셔너리 주요 연산 
+
+score = []
