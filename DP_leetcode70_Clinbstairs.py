896d543a6506032fd79e8a1db9bbbbfec84ffea5
양정우 <yee777222000@gmaill.com>
2024-07-20 10:53:32 +0900

    dp re study

--- DP_leetcode70_Clinbstairs.py
+++ DP_leetcode70_Clinbstairs.py
@@ -1,12 +1,21 @@
+dp = {}
 class Solution(object):
     def climbStairs(self, n):
-        dp = {}
-        dp[0] = 0
-        dp[1] = 1
-        dp[2] = 2
-        for i in range(n+1):
-            if i not in dp:
-                dp[i] = Solution.climbStairs(self,n-2) + Solution.climbStairs(self,n-1)
+        if n==1:
+            return 1
+        if n==2:
+            return 2
+        
+        # 접근 5 층에 접근전 단계는  5-2 , 5 -1 
+        # 즉 DP[n] = dp[n-2] +dp[n-1] 이라는 규칙이 발견 
+        # top down 방식
+        '''
+        if n not in dp:
+            dp[n] = Solution.climbStairs(self,n-2) + Solution.climbStairs(self,n-1)
+        '''
+        # bottom up 방식
+        for i in range(3,n+1):
+            dp[i] = dp[i-2] +dp[i-1]
 
         return dp[n]
-        
+    
